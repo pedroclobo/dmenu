eca3d4aacc43646790952bc88212468deae298f8
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-07-04 21:51:23 +0100

    Change font and colors

--- config.def.h
+++ config.def.h
@@ -1,23 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static int topbar = 1;
+static const char *fonts[] = {"UbuntuMono Nerd Font:size=11"};
+static const char *prompt = NULL;
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*                 fg         bg    */
+    [SchemeNorm] = {"#d8dee9", "#2e3440"},
+    [SchemeSel] = {"#e5e9f0", "#5e81ac"},
+    [SchemeOut] = {"#000000", "#00ffff"},
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
+static unsigned int lines = 0;
 static const char worddelimiters[] = " ";
